596b553c4c893219cd2b05141a704f1bbf670dca
Maximilian Mühlfeld <maggistro@gmx.net>
2015-10-11 14:52:27 +0200

    added socketserver and removed old src

---
+++ CameraServer/Socketmodul.cpp
@@ -0,0 +1,58 @@
+#include Socketmodul.h
+
+
+// constructor
+Socketmodul::Socketmodul(){
+	connectionStatus = false;
+	modulePort = Socketmodul::SOCKETMODUL_PORT;
+	tickRate = Socketmodul::MAX_TICK_RATE;
+	stopConnection = false;
+};
+
+
+// destructor
+Socketmodul::~Socketmodul(){
+
+};
+
+bool Socketmodul::openConnection(){
+	return true;
+};
+
+
+bool Socketmodul::closeConnection(){
+	stopConnection = true;
+
+
+	//TODO: wait for connections to be closed
+
+
+	return true;
+};
+
+
+bool Socketmodul::sendData(PACKAGE package){
+
+
+	return true;
+};
+
+
+bool Socketmodul::receiveData(PACKAGE package){
+
+	return true;
+};
+
+
+void Socketmodul::changeConnectionTickRate(int newTickRate){
+	tickRate = newTickRate;
+};
+
+
+void Socketmodul::printSocketStatus(){
+	fprintf(stderr, "Socketstatus");
+};
+
+void Socketmodul::SocketServerTask(void* arg){
+
+};
---
+++ CameraServer/Socketmodul.h
@@ -0,0 +1,103 @@
+#ifndef SOCKETMODUL_H
+#define SOCKETMODUL_H
+
+// System headers
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "DataTypes.h";
+
+// constant declarations
+#define SOCKETMODUL_PORT 8080
+
+// send rate or timeout
+#define unsigned int MAX_TICK_RATE = 100;
+
+
+#define unsigned int MAX_DATA_LENGTH = 448
+#define unsigned int DATALENGTH_START = 192;
+#define unsigned int DATALENGTH_STOP = 0;
+#define unsigned int DATALENGTH_RESET = 0;
+#define unsigned int DATALENGTH_THUMB_UP = 0;
+#define unsigned int DATALENGTH_THUMB_DOWN = 0;
+#define unsigned int DATALENGTH_FOLLOW = 192;
+#define unsigned int DATALENGTH_INFO = 448;
+#define unsigned int DATALENGTH_STATUS = 64;
+
+#define unsigned int MAX_FAILED_CONNECTION_TRYS = 64;
+
+/**
+* possible instructions
+* "start": motion command for the camera 
+* "stop": motion command for the camera
+* "reset": resets the robot position and gets it into home-position
+* "thumb_up": motion command for the camera
+* "thumb_down": motion command for the camera
+* "follow": follows the hand
+* "info": axis positions
+* "status": feedback from the controller/robot
+*/
+typedef struct{
+	char instructionName[64];
+	unsigned int dataLength;
+}SOCKET_PACKAGE_HEADER;
+
+typedef struct{
+	SOCKET_PACKAGE_HEADER dataHeader;
+	// TODO: DEFINE DATA 
+}SOCKET_PACKAGE;
+
+// instructions that have a body
+typedef struct{
+	unsigned int x;
+	unsigned int y;
+	unsigned int z;
+}INSTRUCTION_START_BODY;
+
+typedef struct{
+	unsigned int x;
+	unsigned int y;
+	unsigned int z;
+}INSTRUCTION_FOLLOW_BODY;
+
+typedef struct{
+	int axis_one;
+	int axis_two;
+	int axis_three;
+	int axis_four;
+	int axis_five;
+	int axis_six;
+	int axis_seven;
+}INSTRUCTION_INFO_BODY;
+
+typedef struct{
+	unsigned int status;
+}INSTRUCTION_STATUS_BODY;
+
+class Socketmodul{
+
+private:
+	bool connectionStatus;
+	unsigned int modulePort;
+	unsigned int tickRate;
+	bool stopConnection;
+	
+
+public:
+	Socketmodul();
+	~Socketmodul();
+
+	bool openConnection();
+	bool closeConnection();	
+	bool sendData(PACKAGE package);
+	bool receiveData(PACKAGE package);
+	void changeConnectionTickRate(int newTickRate);
+	void printSocketStatus();
+
+	//starts the server task
+	void* SocketServerTask(void* arg);
+
+
+};
+
+#endif
