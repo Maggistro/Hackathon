727602473286bfa24a655f3c7752710c7d95f28a
unknown <MUEHLFELD@PC41361.roboter.kuka.de>
2015-10-13 15:34:38 +0200

    Added basic functionallity as example in main.

--- CameraServer/CameraServerProgram.cpp
+++ CameraServer/CameraServerProgram.cpp
@@ -1,4 +1,10 @@
 #include "CameraServerProgram.h"
+#include <pxcsession.h>
+#include <pxcsensemanager.h>
+#include <pxchanddata.h>
+#include <pxchandmodule.h>
+#include <pxchandconfiguration.h>
+#include <iostream>
 
 void main(){
 	//test
@@ -13,4 +19,59 @@ void main(){
 	//maybe additional feedback thread waiting for data from robot
 
 	//wait for join/break on/in one thread
+
+	PXCSession* session = PXCSession::CreateInstance();
+	PXCSession::ImplVersion ver = session->QueryVersion();
+
+	wprintf_s(L"SDK Session startet with version %d.%d\n", ver.major, ver.minor);
+
+	PXCSenseManager* sm = PXCSenseManager::CreateInstance();
+	sm->EnableHand();
+
+	sm->Init();
+
+
+	PXCHandModule *hand;
+	hand = sm->QueryHand();
+
+	PXCHandData *handData;
+	handData = hand->CreateOutput();
+
+	PXCHandConfiguration* config = hand->CreateActiveConfiguration();
+	config->EnableNormalizedJoints(true);
+	config->EnableSegmentationImage(true);
+	config->EnableAllGestures();
+	config->ApplyChanges();
+	config->Update();
+
+	PXCHandData::GestureData gesture;
+
+	while (1)
+	{
+		if (int status = sm->AcquireFrame(true) < PXC_STATUS_NO_ERROR)
+		{
+			wprintf_s(L"no frame %d\n", status);
+			break;
+		}
+
+
+		config->DisableAllGestures();
+		config->Update();
+		handData->Update();
+
+		for (int id = 0; id < handData->QueryFiredGesturesNumber(); id++)
+		{
+			if (handData->QueryFiredGestureData(id, gesture) == PXC_STATUS_NO_ERROR)
+			{
+				wprintf_s(L"%s\n", gesture.name);
+			}
+		}
+		config->DisableAllGestures();
+		config->Update();
+		sm->ReleaseFrame();
+	}
+
+	sm->Release();
+
+	session->Release();
 }
