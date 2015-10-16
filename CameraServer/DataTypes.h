27510f86f9c4c97d5382215db040a0eab003fa02
klugoutk <klugout@gmail.com>
2015-10-16 20:16:33 +0200

    camera optimization

--- CameraServer/DataTypes.h
+++ CameraServer/DataTypes.h
@@ -11,13 +11,17 @@ typedef struct {
 }instruction_package;
 
 typedef enum {
-	start = 3,
+	start = 3,// 3 mal integer
 	stop = 0,
 	reset = 0,
+	// optional
 	thump_up = 0,
+	// optional
 	thump_down = 0,
 	follow = 3,
+	// optional
 	info = 7,
+	// optional
 	status = 1,
 }headers;
 
